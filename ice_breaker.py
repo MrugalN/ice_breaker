0bf0c09c447a28806199d33507d475bb3a421947
MrugalN <mrugalnikhar@uconn.edu>
2024-06-24 00:19:47 -0400

    Base of the Application

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,12 +4,13 @@ from langchain_openai import ChatOpenAI
 from third_parties.linkedin import scrape_linkedin_profile
 from langchain_core.output_parsers import StrOutputParser
 import os
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agents import lookup as linkedin_lookup_agent
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
@@ -31,3 +32,11 @@ if __name__ == "__main__":
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+    
+if __name__ == "__main__":
+    load_dotenv()
+    
+    print("Ice Breaker Enter")
+    ice_break_with(name="Mrugal Nikhar")
+    
+   
